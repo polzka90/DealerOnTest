012677f143b5080ae007909a45274a3c664fd8b3
polzka90 <leopoldo.urdaneta@gmail.com>
2021-09-27 21:43:04 -0300

    add readme unit test example

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # DealerOnTest
+
+## Requirements
+
+ 1. Git to clone the project
+ 2. Visual Studio to restore and build
+ 3. Postman
+
+## Brief Explanation
+
+FunctionApp: is the API to access to the backend, 
+			 i use a Azure Function the get it ready to deploy to azure
+
+Application: is where i put the bussiness rules and logics, using UseCase for each context
+
+Infrastructure: is where generally i put Acl, Repository, Services, etc
---
+++ UnitTesting/UseCase/MarsRoversCommandHandlerTest.cs
@@ -0,0 +1,65 @@
+﻿using Application.UseCases.MarsRovers;
+using Application.UseCases.MarsRovers.Models.Commands;
+using Application.UseCases.MarsRovers.Models.Outputs;
+using FluentAssertions;
+using Infrastructure.CardinalMap;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting.UseCase
+{
+    public class MarsRoversCommandHandlerTest
+    {
+        private readonly MarsRoversCommandHandler marsRoversCommandHandler;
+        private readonly Mock<ILogger<MarsRoversCommandHandler>> logger;
+        public MarsRoversCommandHandlerTest()
+        {
+            logger = new Mock<ILogger<MarsRoversCommandHandler>>();
+            marsRoversCommandHandler = new MarsRoversCommandHandler(logger.Object, new CardinalMap(
+                new CardinalChain()
+                ));
+        }
+
+        [Fact]
+        public async Task ShouldBeReturnAValidOutputAndCorrectResult()
+        {
+            MarsRoversCommand marsRoversCommand = new MarsRoversCommand()
+            {
+                LimitX = 5,
+                LimitY = 5,
+                Rovers = new List<RoverCommand>()
+                {
+                    new RoverCommand()
+                    {
+                        PositionX = 1,
+                        PositionY = 2,
+                        PositionZ = 'N',
+                        Command = "LMLMLMLMM"
+                    }
+                }
+            };
+            var output = await marsRoversCommandHandler.Handle(marsRoversCommand, new System.Threading.CancellationToken());
+
+            output.IsValid.Should().BeTrue();
+            output.ErrorNessages.Should().BeNullOrEmpty();
+
+            output.Result.Should().NotBeNull();
+
+            MarsRoversResultOutput marsRoversResultOutput = (MarsRoversResultOutput)output.Result;
+
+            marsRoversResultOutput.Rovers.Should().NotBeNullOrEmpty();
+            marsRoversResultOutput.Rovers.Count.Should().BeGreaterThan(0);
+
+            var rover = marsRoversResultOutput.Rovers.FirstOrDefault();
+
+            rover.Position.Should().Be("1 3 N");
+
+        }
+    }
+}
